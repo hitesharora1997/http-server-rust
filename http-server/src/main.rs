617c4210b278e6a68ffc78627b28de3a44ed64e6
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-11-03 18:06:38 +0100

    new

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    let _get = Method::GET("hello".to_string());
+    let _post = Method::POST;
+    let _patch = Method::PATCH;
+    let _put = Method::PUT;
+    let _head = Method::HEAD;
+    let _connect = Method::CONNECT;
+    let _delete = Method::DELETE(100);
     let server = Server::new(String::from("127.0.0.1:8080").to_string());
 
     // dbg!(server);
@@ -26,9 +33,21 @@ impl Server {
         println!("Running the server on {}", self.addr);
     }
 }
-
+// #[derive(Debug)]
 struct Request {
     path: String,
     query_string: String,
-    method: String,
+    method: Method,
+}
+// #[derive(Debug)]
+enum Method {
+    GET(String),
+    POST,
+    PUT,
+    DELETE(u64),
+    CONNECT,
+    OPTIONS,
+    HEAD,
+    TRACE,
+    PATCH,
 }
