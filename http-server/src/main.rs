3e019131909aa5d8be6fe68dc5abb0f05924fc7d
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-11-03 18:58:29 +0100

    new

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -1,11 +1,14 @@
+use http::request::Request;
+use server::Server;
+
 fn main() {
-    let _get = Method::GET("hello".to_string());
-    let _post = Method::POST;
-    let _patch = Method::PATCH;
-    let _put = Method::PUT;
-    let _head = Method::HEAD;
-    let _connect = Method::CONNECT;
-    let _delete = Method::DELETE(100);
+    let _get = http::method::Method::GET("hello".to_string());
+    // let _post = Method::POST;
+    // let _patch = Method::PATCH;
+    // let _put = Method::PUT;
+    // let _head = Method::HEAD;
+    // let _connect = Method::CONNECT;
+    // let _delete = Method::DELETE(100);
     let server = server::Server::new(String::from("127.0.0.1:8080").to_string());
 
     // dbg!(server);
@@ -37,35 +40,30 @@ mod server {
     }
 }
 
-// #[derive(Debug)]
-// struct Server {
-//     addr: String,
-// }
+mod http {
 
-// impl Server {
-//     fn new(addr: String) -> Self {
-//         Self { addr: addr }
-//     }
+    pub mod request {
+        // #[derive(Debug)]
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: super::method::Method,
+        }
+    }
 
-//     fn run(self) {
-//         println!("Running the server on {}", self.addr);
-//     }
-// }
-#[derive(Debug)]
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-#[derive(Debug)]
-enum Method {
-    GET(String),
-    POST,
-    PUT,
-    DELETE(u64),
-    CONNECT,
-    OPTIONS,
-    HEAD,
-    TRACE,
-    PATCH,
+    pub mod method {
+
+        // #[derive(Debug)]
+        pub enum Method {
+            GET(String),
+            POST,
+            PUT,
+            DELETE(u64),
+            CONNECT,
+            OPTIONS,
+            HEAD,
+            TRACE,
+            PATCH,
+        }
+    }
 }
