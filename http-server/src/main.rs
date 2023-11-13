173c5f631cac7842bc7fb5830f4be76099e242ac
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-11-13 14:59:48 +0100

    latest

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -1,5 +1,6 @@
 mod http;
 mod server;
+
 fn main() {
     let server = server::Server::new(String::from("127.0.0.1:8080").to_string());
 
--- http-server/src/server.rs
+++ http-server/src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 #[derive(Debug)]
 pub struct Server {
     addr: String,
@@ -10,5 +13,23 @@ impl Server {
 
     pub fn run(self) {
         println!("Running the server on {}", self.addr);
+
+        // Recoverable error
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buf = [0; 1024];
+                    match stream.read(&mut buf) {
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buf));
+                        }
+                        Err(e) => println!("failed to read a connection {}", e),
+                    }
+                }
+                Err(e) => println!("failed to establish connection error {}", e),
+            }
+        }
     }
 }
