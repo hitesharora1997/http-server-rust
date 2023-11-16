aacb28de5d6f21cd9b5981517b0bec5d6fe6462f
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-11-16 10:46:07 +0100

    new

--- http-server/src/http/request.rs
+++ http-server/src/http/request.rs
@@ -2,6 +2,8 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 #[derive(Debug)]
 pub struct Request {
     path: String,
@@ -13,7 +15,8 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!();
+        let request = str::from_utf8(buf)?;
+        unimplemented!()
     }
 }
 
@@ -47,5 +50,11 @@ impl Display for ParseError {
 impl Debug for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
