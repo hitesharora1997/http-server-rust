5a4a2b9797d0548a7e56e36742aa90cee3323f44
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-11-16 22:38:22 +0100

    new

--- http-server/target/debug/deps/http_server-a825d87fa2ac13f2.d
+++ http-server/target/debug/deps/http_server-a825d87fa2ac13f2.d
@@ -1,9 +1,10 @@
-/home/hitesh/Rust/course/http.server/http-server/target/debug/deps/http_server-a825d87fa2ac13f2.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
+/home/hitesh/Rust/course/http.server/http-server/target/debug/deps/http_server-a825d87fa2ac13f2.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/server.rs
 
-/home/hitesh/Rust/course/http.server/http-server/target/debug/deps/http_server-a825d87fa2ac13f2.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
+/home/hitesh/Rust/course/http.server/http-server/target/debug/deps/http_server-a825d87fa2ac13f2.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/server.rs
 
 src/main.rs:
 src/http/mod.rs:
 src/http/method.rs:
+src/http/query_string.rs:
 src/http/request.rs:
 src/server.rs:
