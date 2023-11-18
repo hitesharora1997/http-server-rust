d63a880fd3f777a2b0ca9f322054a8823b0e1ffc
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-11-18 18:12:35 +0100

    new

--- http-server/src/website_handler.rs
+++ http-server/src/website_handler.rs
@@ -1,13 +1,23 @@
-use crate::http::Response;
+use crate::http::{Method, Response};
 
 use super::server::Handler;
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &crate::http::Request) -> Response {
-        Response::new(
-            crate::http::StatusCode::Ok,
-            Some("<h1>TEST</h1>".to_string()),
-        )
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(
+                    crate::http::StatusCode::Ok,
+                    Some("<h1>Welcome</h1>".to_string()),
+                ),
+                "/hello" => Response::new(
+                    crate::http::StatusCode::Ok,
+                    Some("<h1>hello</h1>".to_string()),
+                ),
+                _ => Response::new(crate::http::StatusCode::NotFound, None),
+            },
+            _ => Response::new(crate::http::StatusCode::NotFound, None),
+        }
     }
 }
